12b6d285fac5867f4dd327bf735679dadd983008
Jason Furtney <jkfurtney@gmail.com>
2012-06-23 12:51:14 -0500

    adding git setup script

---
+++ git_setup.sh
@@ -0,0 +1,7 @@
+git config --global user.name "Jason Furtney"
+git config --global user.email "jkfurtney@gmail.com"
+
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.interactive auto
+git config --global color.status auto
