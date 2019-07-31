36a225d9329266b5b95484d37afe9a34efbff9ff
Jason Furtney <jkfurtney@gmail.com>
2019-07-31 14:26:22 -0500

    support functions for (human) reading of TOUGH3 input files

--- tough3.el
+++ tough3.el
@@ -1,6 +1,7 @@
 (require 'generic-x)
 
-(defconst tough3-mode-keywords '(ROCKS TRANS START PARAM RPCAP ELEME CONNE INCON GENER FOFT GOFT OUTPU ENDCY FRACT CONBD SOLVR MULTI MESHMAKER1 ENDFI MULTI))
+(defconst tough3-mode-keywords '(ROCKS START PARAM RPCAP ELEME CONNE INCON GENER FOFT GOFT OUTPU ENDCY FRACT CONBD SOLVR MULTI MESHMAKER1 ENDFI DIFFU COFT HYSTE TIMES INDOM TIMBC))
+
 
 
 (define-generic-mode 'tough3-mode
@@ -13,3 +14,27 @@
   (list (lambda ()
           (set (make-local-variable 'mode-name) "Tough3")))
   "Mode for Tough3 data files")
+
+(defvar tough3-columns-colors '("#DCDCDC" "#808080" "#404040"))
+(defvar tough3-color  "#808080")
+
+(defun tough3-highlight-region (boundaries)
+  (put-text-property (car boundaries) (cdr boundaries)
+                     'font-lock-face `(:background ,tough3-color)))
+
+(defun tough3-stripe-current-line (numbers)
+  (let* ((current-char 0)
+         (show 0))
+    (dotimes (i (length numbers))
+      (if (and (< (+ (point) current-char) (point-at-eol)) (evenp show))
+          (tough3-highlight-region (cons (+ (point-at-bol) current-char) (+ (point-at-bol) current-char (elt numbers i)))))
+      (incf show)
+      (incf current-char (elt numbers i)))))
+
+(defun jkf/uf-region (a b)
+  (interactive "r")
+  (font-lock-unfontify-region a b))
+
+(defun ufine () (interactive)
+       (font-lock-unfontify-region (point-at-bol) (point-at-eol)))
+;(tough3-stripe-current-line '(3 2 5 5 3 2 10 10 10 10))
