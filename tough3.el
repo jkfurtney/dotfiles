923b2ea6eb23dd34ea2d6d42e4f56c62146e754e
Jason Furtney <jkfurtney@gmail.com>
2019-07-31 16:12:14 -0500

    more formatting for fixed length lines

--- tough3.el
+++ tough3.el
@@ -1,6 +1,6 @@
 (require 'generic-x)
 
-(defconst tough3-mode-keywords '(ROCKS START PARAM RPCAP ELEME CONNE INCON GENER FOFT GOFT OUTPU ENDCY FRACT CONBD SOLVR MULTI MESHMAKER1 ENDFI DIFFU COFT HYSTE TIMES INDOM TIMBC))
+(defconst tough3-mode-keywords '(ROCKS START PARAM RPCAP ELEME CONNE INCON GENER FOFT GOFT OUTPU ENDCY SOLVR MULTI MESHMAKER1 ENDFI DIFFU COFT HYSTE TIMES INDOM TIMBC))
 
 
 
@@ -15,19 +15,20 @@
           (set (make-local-variable 'mode-name) "Tough3")))
   "Mode for Tough3 data files")
 
-(defvar tough3-columns-colors '("#DCDCDC" "#808080" "#404040"))
-(defvar tough3-color  "#808080")
+(defconst tough3-color-1  "#808080")
+(defconst tough3-color-2  "#669999")
 
-(defun tough3-highlight-region (boundaries)
+(defun tough3-highlight-region (boundaries color)
   (put-text-property (car boundaries) (cdr boundaries)
-                     'font-lock-face `(:background ,tough3-color)))
+                     'font-lock-face `(:background ,color)))
 
 (defun tough3-stripe-current-line (numbers)
   (let* ((current-char 0)
          (show 0))
     (dotimes (i (length numbers))
-      (if (and (< (+ (point) current-char) (point-at-eol)) (evenp show))
-          (tough3-highlight-region (cons (+ (point-at-bol) current-char) (+ (point-at-bol) current-char (elt numbers i)))))
+      (if (<= (+ (point) current-char (elt numbers i)) (point-at-eol))
+          (progn (tough3-highlight-region (cons (+ (point-at-bol) current-char) (+ (point-at-bol) current-char (elt numbers i)))
+                                          (if (evenp show) tough3-color-1 tough3-color-2))))
       (incf show)
       (incf current-char (elt numbers i)))))
 
@@ -37,4 +38,9 @@
 
 (defun ufine () (interactive)
        (font-lock-unfontify-region (point-at-bol) (point-at-eol)))
-;(tough3-stripe-current-line '(3 2 5 5 3 2 10 10 10 10))
+(tough3-stripe-current-line '(3 2 5 5 3 2 10 10 10 10))
+
+
+(defconst tough3-fmt-ELEM '(5 5 5 5 10 10 10 10 10 10 10 10 10))
+(defconst tough3-fmt-CONNE '(5 5 5 5 5 5 10 10 10 10 10))
+(defconst tough3-fmt-PARAM-1 '(2 2 4 4 4 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 10 10 10))
